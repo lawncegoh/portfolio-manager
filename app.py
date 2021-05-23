43efa3f3318fa442d918c789788c1a43c2ef0d1a
Lawnce Goh <lawnce@gmail.com>
2021-05-23 20:39:33 +0800

    did some refresh shit

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, render_template, request, redirect, url_for
 from yahoo_fin.stock_info import get_data
+import logging
 
 app = Flask(__name__)
 
@@ -57,21 +58,25 @@ def delete(fund_id):
     db.session.commit()
     return redirect(url_for("home"))
 
-@app.route("/refresh/?start_date=<string:start_date>&end_date=<string:end_date>", methods=["POST"])
+@app.route("/refresh/", methods=["get"])
 def refresh():
-    for all in Fund.query.all():
-        print(all)
-    # amazon_weekly= get_data("xlf", start_date=request.form.get("start_date"), end_date=request.form.get("end_date"), index_as_date = True, interval="1wk")
-    # ticker_list = ["SPY", "IUSV", "XLR"]
-    # historical_datas = {}
-    # for ticker in ticker_list:
-    #     historical_datas[ticker] = get_data(ticker)
+    fund_list = Fund.query.all()
+
+    ticker_list = []
+    for each in fund_list:
+        ticker_list.append(each.name)
+
+    historical_datas = {}
+    for ticker in ticker_list:
+        historical_datas[ticker] = get_data(ticker)
+
+    print(historical_datas['XLF'])
+    #use historical_data to do math
 
     #### use a ticker list then get data for the entire portfolio
+    # amazon_weekly= get_data("xlf", start_date=request.form.get("start_date"), end_date=request.form.get("end_date"), index_as_date = True, interval="1wk")
 
-    # refresh based on the start and end date, using this df to come up with a total returns value or chart first
-    # amazon_weekly= get_data("xlf", start_date=start_date, end_date=end_date, index_as_date = True, interval="1wk")
-    # print(amazon_weekly)
+    return redirect(url_for("home"))
 
 
 if __name__ == "__main__":
